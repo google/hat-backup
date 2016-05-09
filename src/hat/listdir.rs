f376a37aaa1876db50e73eb4019b97cec8f42140
Johan Brinch <brinchj@gmail.com>
2016-05-09 23:44:17 +0200

    Add basic test for iterate_recursively.

--- src/hat/listdir.rs
+++ src/hat/listdir.rs
@@ -145,3 +145,116 @@ impl PathHandler<()> for PrintPathHandler {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::collections::btree_map;
+    use std::io;
+    use std::path::PathBuf;
+    use std::sync::{Arc, Mutex};
+    use std::vec;
+
+    use super::*;
+
+    type ParentOpt = Option<PathBuf>;
+    type VisitedPaths = btree_map::BTreeMap<PathBuf, bool>;
+
+    impl HasPath for PathBuf {
+        fn path(&self) -> PathBuf {
+            self.to_owned()
+        }
+    }
+
+    #[derive(Clone)]
+    struct StubPathHandler {
+        paths: Arc<Mutex<VisitedPaths>>,
+    }
+
+    impl StubPathHandler {
+        fn new(paths: Vec<PathBuf>) -> StubPathHandler {
+            let mut tree = btree_map::BTreeMap::new();
+            for path in paths {
+                tree.insert(path, false);
+            }
+            StubPathHandler { paths: Arc::new(Mutex::new(tree)) }
+        }
+
+        fn visit(&self, path: PathBuf) -> Option<bool> {
+            let mut paths = self.paths.lock().unwrap();
+            paths.insert(path, true)
+        }
+
+        fn list(&self, dir: &PathBuf) -> vec::IntoIter<io::Result<PathBuf>> {
+            let paths = self.paths.lock().unwrap();
+            let mut contents = vec![];
+            for k in paths.keys() {
+                if k.parent() == Some(dir) {
+                    contents.push(Ok(k.clone()));
+                }
+            }
+            contents.into_iter()
+        }
+
+        fn not_visited(&self) -> Vec<PathBuf> {
+            let paths = self.paths.lock().unwrap();
+            let mut contents = vec![];
+            for (path, visited) in paths.iter() {
+                if !visited {
+                    contents.push(path.clone());
+                }
+            }
+            contents
+        }
+    }
+
+    impl PathHandler<ParentOpt> for StubPathHandler {
+        type DirIter = vec::IntoIter<io::Result<PathBuf>>;
+
+        fn read_dir(&self, path: &PathBuf) -> io::Result<Self::DirIter> {
+            Ok(self.list(path))
+        }
+
+        fn handle_path(&self, p_opt: ParentOpt, path: PathBuf) -> Option<ParentOpt> {
+            assert_eq!(self.visit(path.clone()), Some(false));
+
+            if let Some(p) = p_opt {
+                assert!(path.parent() == Some(&p));
+            }
+
+            self.list(&path).next().map(|_| Some(path))
+        }
+    }
+
+    #[test]
+    fn can_visit_all() {
+        let paths: [&str; 20] = ["/",
+                                 "/foo",
+                                 "/bar/",
+                                 "/bar/baz/",
+                                 "/bar/baz/qux",
+                                 "/bar/baz/foo",
+                                 "/bar/baz/bar/",
+                                 "/bar/baz/bar/foo",
+                                 "/bar/baz/bar/bar/",
+                                 "/bar/baz/bar/bar/bar",
+                                 "/empty/",
+                                 "/empty/1/",
+                                 "/empty/2/",
+                                 "/empty/3/",
+                                 "/empty/4/",
+                                 "/empty/5/",
+                                 "/empty/6/",
+                                 "/empty/7/",
+                                 "/empty/8/",
+                                 "/empty/9/",
+                                 ];
+
+        let mut handler = StubPathHandler::new(paths.iter().map(PathBuf::from).collect());
+        iterate_recursively((PathBuf::from("/"), None), &mut handler);
+
+        assert_eq!(handler.not_visited(), vec![PathBuf::from("/")]);
+    }
+
+}
