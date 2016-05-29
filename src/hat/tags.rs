53dccec13f4d10fefb636c0700233bf0f3866692
Mathias Svensson <idolf@google.com>
2016-05-29 21:13:28 +0200

    Made the empty lines after the copyright header more consistent

--- src/hat/tags.rs
+++ src/hat/tags.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Tag {
     Done = 0,
